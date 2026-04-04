d00cd2ee3cf96a880f444e0e0a49f035e753a110
Brian Madroñero <brainmadro@gmail.com>
2026-04-04 22:32:59 +1300

    fix: :bug: Add the Team activity changes for W04

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
 
--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,4 +1,5 @@
 const utilities = require(".");
+const accountModel = require("../models/account-model");
 const { body, validationResult } = require("express-validator");
 const validate = {};
 
@@ -26,28 +27,28 @@ validate.loginRules = () => {
         minSymbols: 1,
       })
       .withMessage("Password does not meet requirements."),
-  ]
-}
+  ];
+};
 
 /* ******************************
  * Check data and return errors or continue to login
  * ***************************** */
 validate.checkLoginData = async (req, res, next) => {
-  const { account_email } = req.body
-  let errors = []
-  errors = validationResult(req)
+  const { account_email } = req.body;
+  let errors = [];
+  errors = validationResult(req);
   if (!errors.isEmpty()) {
-    let nav = await utilities.getNav()
+    let nav = await utilities.getNav();
     res.render("account/login", {
       errors,
       title: "Login",
       nav,
       account_email,
-    })
-    return
+    });
+    return;
   }
-  next()
-}
+  next();
+};
 
 /*  **********************************
  *  Registration Data Validation Rules
@@ -70,14 +71,21 @@ validate.registationRules = () => {
       .isLength({ min: 2 })
       .withMessage("Please provide a last name."), // on error this message is sent.
 
-    // valid email is required and cannot already exist in the DB
+    // valid email is required and cannot already exist in the database
     body("account_email")
       .trim()
       .escape()
       .notEmpty()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists =
+          await accountModel.checkExistingEmail(account_email);
+        if (emailExists) {
+          throw new Error("Email exists. Please log in or use different email");
+        }
+      }),
 
     // password is required and must be strong password
     body("account_password")
@@ -98,11 +106,11 @@ validate.registationRules = () => {
  * Check data and return errors or continue to registration
  * ***************************** */
 validate.checkRegData = async (req, res, next) => {
-  const { account_firstname, account_lastname, account_email } = req.body
-  let errors = []
-  errors = validationResult(req)
+  const { account_firstname, account_lastname, account_email } = req.body;
+  let errors = [];
+  errors = validationResult(req);
   if (!errors.isEmpty()) {
-    let nav = await utilities.getNav()
+    let nav = await utilities.getNav();
     res.render("account/register", {
       errors,
       title: "Registration",
@@ -110,10 +118,10 @@ validate.checkRegData = async (req, res, next) => {
       account_firstname,
       account_lastname,
       account_email,
-    })
-    return
+    });
+    return;
   }
-  next()
-}
+  next();
+};
 
-module.exports = validate
+module.exports = validate;
